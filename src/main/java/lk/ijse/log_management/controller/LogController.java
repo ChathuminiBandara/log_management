effd532ea357045c579bffd23508984e90a48066
ChathuminiBandara <chathubandara500@gmail.com>
2025-03-03 11:52:58 +0530

    Log variations

--- src/main/java/lk/ijse/log_management/controller/LogController.java
+++ src/main/java/lk/ijse/log_management/controller/LogController.java
@@ -1,2 +1,28 @@
-package lk.ijse.log_management.controller;public class LogController {
+package lk.ijse.log_management.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LogController.class);
+
+    @GetMapping("/log")
+    String getLog(){
+        logger.info("Info log : ");
+        logger.warn("Warn log : ");
+        logger.error("Error log : ");
+        logger.debug("{} Debug log : ","Hello");
+
+
+        try{
+            throw new RuntimeException("Exception log : ");
+        } catch (RuntimeException e) {
+            logger.error("You have RuntimeException log : {} ", e.getMessage());
+        }
+        return "Log Message Generated !";
+    }
 }
